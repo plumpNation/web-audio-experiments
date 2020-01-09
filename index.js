5a3950b22edaa56b690e2a7a8aea1894d369b123
Gavin King <gavin.king@acando.no>
2020-01-10 00:26:34 +0100

    Got a sequence in there now

--- index.js
+++ index.js
@@ -1,17 +1,58 @@
 const btn = document.querySelector('#play');
 
-btn.addEventListener('click', () => {
-  runThatBadBody();
+const tempo = 100;
+
+const notes = [
+  [659, 4],
+  [659, 4],
+  [659, 4],
+  [523, 8],
+  [0, 16],
+  [783, 16],
+  [659, 4],
+  [523, 8],
+  [0, 16],
+  [783, 16],
+  [659, 4],
+  [0, 4],
+  [987, 4],
+  [987, 4],
+  [987, 4],
+  [1046, 8],
+  [0, 16],
+  [783, 16],
+  [622, 4],
+  [523, 8],
+  [0, 16],
+  [783, 16],
+  [659, 4]
+];
+
+notes.reverse(); // reverse so we can use pop instead of shift for perf reasons probs
+
+btn.addEventListener('mousedown', () => {
+  playMelody();
 });
 
-function runThatBadBody() {
+function playMelody() {
+  if (notes.length > 0) {
+    note = notes.pop();
+    playNote(note[0], 256/(note[1]*tempo), playMelody);
+  }
+}
+
+// /////////////////////////////////////////////////////////////////////////////
+// /////////////////////////////////////////////////////////////////////////////
+// /////////////////////////////////////////////////////////////////////////////
+
+function playNote(frequency, duration, callback) {
   var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 
   /** @type OscillatorNode */
   const oscillator = audioCtx.createOscillator();
 
   oscillator.type = 'square';
-  oscillator.frequency.setValueAtTime(50, audioCtx.currentTime);
+  oscillator.frequency.value = frequency;
 
   //set up the different audio nodes we will use for the app
   var analyser = audioCtx.createAnalyser();
@@ -37,30 +78,42 @@ function runThatBadBody() {
 
   biquadFilter.type = "lowpass";
   biquadFilter.frequency.setValueAtTime(1000, audioCtx.currentTime);
-  biquadFilter.frequency.setValueAtTime(200, audioCtx.currentTime + 2);
   biquadFilter.gain.setValueAtTime(25, audioCtx.currentTime);
 
   oscillator.start();
-
-  setTimeout(() => {
-    oscillator.stop();
-  }, 3000);
+  oscillator.stop(audioCtx.currentTime + duration);
+  oscillator.onended = callback;
 }
 
-
+/**
+ * It's in stereo too!
+ *
+ * Thanks to https://stackoverflow.com/users/1789439/cwilso
+ * @see https://stackoverflow.com/questions/22525934/connecting-convolvernode-to-an-oscillatornode-with-the-web-audio-the-simple-wa
+ *
+ * @param {AudioContext} audioCtx
+ * @param {number} duration
+ * @param {number} decay
+ * @param {boolean} reverse
+ * @returns {AudioBuffer}
+ */
 function impulseResponse( audioCtx, duration, decay, reverse ) {
-  var sampleRate = audioCtx.sampleRate;
-  var length = sampleRate * duration;
-  var impulse = audioCtx.createBuffer(2, length, sampleRate);
-  var impulseL = impulse.getChannelData(0);
-  var impulseR = impulse.getChannelData(1);
+  const sampleRate = audioCtx.sampleRate;
+  const length = sampleRate * duration;
+  const impulse = audioCtx.createBuffer(2, length, sampleRate);
+  const impulseL = impulse.getChannelData(0);
+  const impulseR = impulse.getChannelData(1);
 
-  if (!decay)
-      decay = 2.0;
-  for (var i = 0; i < length; i++){
-    var n = reverse ? length - i : i;
+  if (!decay) {
+    decay = 2.0;
+  }
+
+  for (let i = 0; i < length; i += 1) {
+    const n = reverse ? length - i : i;
+
     impulseL[i] = (Math.random() * 2 - 1) * Math.pow(1 - n / length, decay);
     impulseR[i] = (Math.random() * 2 - 1) * Math.pow(1 - n / length, decay);
   }
+
   return impulse;
 }
